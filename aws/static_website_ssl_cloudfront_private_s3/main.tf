f4e649d71afa8a172a7e82f414cbee76c7ec7cf8
Umach2021 <88324098+Umach2021@users.noreply.github.com>
2022-04-04 18:20:02 +0530

    Update main.tf

--- aws/static_website_ssl_cloudfront_private_s3/main.tf
+++ aws/static_website_ssl_cloudfront_private_s3/main.tf
@@ -27,7 +27,7 @@ enabled = true
 resource "aws_s3_bucket_public_access_block" "website_bucket" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls   = true
+  block_public_acls   = false
   block_public_policy = true
 }
 
