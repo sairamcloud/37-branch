29afa3ccd01d3c944cfab760052ef9ceb32e6e37
Umach2021 <88324098+Umach2021@users.noreply.github.com>
2022-04-20 18:36:44 +0530

    Update shared.tf

--- azure/azure_linux_docker_app_service/shared.tf
+++ azure/azure_linux_docker_app_service/shared.tf
@@ -21,9 +21,9 @@ resource "azurerm_key_vault" "current" {
   resource_group_name = data.azurerm_resource_group.current.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
-  soft_delete_enabled        = true
+  soft_delete_enabled        = false
   soft_delete_retention_days = 7
-  purge_protection_enabled   = false
+  purge_protection_enabled   = true
 
   sku_name = "standard"
 }
